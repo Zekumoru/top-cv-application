5da1b6e968415d1732222daa7c2ffa70692513bf
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-07 15:28:12 +0100

    Add and style next button

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,6 +7,9 @@ class Form extends React.Component {
       <form className="Form">
         <h1 className="title">{this.props.title}</h1>
         <div className="Form-content">{this.props.children}</div>
+        <div className="buttons">
+          <button className="next-btn">Next</button>
+        </div>
       </form>
     );
   }
