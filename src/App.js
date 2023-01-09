b2e0ed2c12c129a58fec0f455e4a27397403c2e0
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-09 21:11:27 +0100

    Create pages arrays object

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import Form from './components/Form';
 import TopNavBar from './components/TopNavBar';
 import LabeledInput from './components/LabeledInput';
 
+const pages = [
+  {
+    title: 'Personal Details',
+    content: [
+      <LabeledInput id="first-name" label="First Name" type="text" />,
+      <LabeledInput id="last-name" label="Last Name" type="text" />,
+      <LabeledInput id="email" label="Email" type="email" />,
+      <LabeledInput id="phone-number" label="Phone Number" type="tel" />,
+    ],
+  },
+  {
+    title: 'Education',
+    content: [
+      <LabeledInput id="title-of-study" label="Title of Study" type="text" />,
+      <LabeledInput id="school-name" label="School Name" type="text" />,
+      <LabeledInput id="from-year" label="From" type="dropdown" />,
+      <LabeledInput id="to-year" label="To" type="dropdown" />,
+    ],
+  },
+];
+
 class App extends React.Component {
+  currentPageIndex = 1;
+
   render() {
+    const { content } = pages[this.currentPageIndex];
+
     return (
       <div className="App">
         <TopNavBar />
-        <Form title="Personal Details">
-          <LabeledInput id="first-name" label="First Name" type="text" />
-          <LabeledInput id="last-name" label="Last Name" type="text" />
-          <LabeledInput id="email" label="Email" type="email" />
-          <LabeledInput id="phone-number" label="Phone Number" type="tel" />
-        </Form>
+        <Form title="Personal Details">{content}</Form>
       </div>
     );
   }
