3feca9d0e99cfb598b65f916fa25aebecf530e54
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-19 16:53:14 +0100

    Fix div to li

--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -4,7 +4,7 @@ import '../styles/TaskListItem.scss';
 class TaskListItem extends React.Component {
   render() {
     const { task } = this.props;
-    return <div className="TaskListItem">{task}</div>;
+    return <li className="TaskListItem">{task}</li>;
   }
 }
 
