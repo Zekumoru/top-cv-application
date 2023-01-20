c2d3ac2b0d4d4d9c273f210a55bbc7abecf96b58
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-20 20:13:18 +0100

    Implement delete on list containers

--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -1,10 +1,25 @@
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import '../styles/TaskListItem.scss';
 
 class TaskListItem extends React.Component {
+  delete = () => {
+    this.props.onDelete(this.props.id);
+  };
+
   render() {
     const { task } = this.props;
-    return <li className="TaskListItem">{task}</li>;
+    return (
+      <li className="TaskListItem">
+        {task}{' '}
+        <FontAwesomeIcon
+          onClick={this.delete}
+          className="delete-icon"
+          icon={faTrash}
+        />
+      </li>
+    );
   }
 }
 
