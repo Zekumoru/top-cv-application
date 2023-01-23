73c46d4cca45c17fb7391d4203c18d12c1be8ed3
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-23 13:56:03 +0100

    Add favicon and credits to it

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,27 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="Footer">
-        Made by Zekumoru{' '}
-        <a
-          href="https://github.com/Zekumoru/top-cv-application"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <FontAwesomeIcon className="icon" icon={faGithub} />
-        </a>
+        <p className="made-by">
+          Made by Zekumoru{' '}
+          <a
+            href="https://github.com/Zekumoru/top-cv-application"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FontAwesomeIcon className="icon" icon={faGithub} />
+          </a>
+        </p>
+        <p className="credits">
+          CV favicon icon by{' '}
+          <a
+            href="https://www.flaticon.com/free-icon/resume_3024497"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            lutfix
+          </a>{' '}
+          on Flaticon
+        </p>
       </footer>
     );
   }
