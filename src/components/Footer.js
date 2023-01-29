73348a94ed6565aae5c3673ba27d6202aa7ffaba
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-29 20:29:54 +0100

    Refactor Footer to functional component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,34 +3,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import '../styles/Footer.scss';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="Footer">
-        <p className="made-by">
-          Made by Zekumoru{' '}
-          <a
-            href="https://github.com/Zekumoru/top-cv-application"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <FontAwesomeIcon className="icon" icon={faGithub} />
-          </a>
-        </p>
-        <p className="credits">
-          CV favicon icon by{' '}
-          <a
-            href="https://www.flaticon.com/free-icon/resume_3024497"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            lutfix
-          </a>{' '}
-          on Flaticon
-        </p>
-      </footer>
-    );
-  }
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p className="made-by">
+        Made by Zekumoru{' '}
+        <a
+          href="https://github.com/Zekumoru/top-cv-application"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <FontAwesomeIcon className="icon" icon={faGithub} />
+        </a>
+      </p>
+      <p className="credits">
+        CV favicon icon by{' '}
+        <a
+          href="https://www.flaticon.com/free-icon/resume_3024497"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          lutfix
+        </a>{' '}
+        on Flaticon
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
