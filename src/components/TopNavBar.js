369fa3f6ac21a6de8558f9a757560bcd84f8c149
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-29 20:46:23 +0100

    Refactor TopNavBar to functional form

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../styles/TopNavBar.scss';
 
-class TopNavBar extends React.Component {
-  render() {
-    return (
-      <div className="TopNavBar">
-        <div className="app-title">CV Builder</div>
-      </div>
-    );
-  }
+function TopNavBar() {
+  return (
+    <div className="TopNavBar">
+      <div className="app-title">CV Builder</div>
+    </div>
+  );
 }
 
 export default TopNavBar;
---
+++ src/components/TopNavBarClass.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/TopNavBar.scss';
+
+class TopNavBarClass extends React.Component {
+  render() {
+    return (
+      <div className="TopNavBar">
+        <div className="app-title">CV Builder</div>
+      </div>
+    );
+  }
+}
+
+export default TopNavBarClass;
